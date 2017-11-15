104ee9e0d78afe8373bff56bf5bf464b878f81c3
Marek Olszewski <marek@godaddy.com>
2017-11-14 18:21:21 -0800

    .

--- android/src/main/java/com/reactnativegeth/RNGethModule.java
+++ android/src/main/java/com/reactnativegeth/RNGethModule.java
@@ -421,7 +421,7 @@ public class RNGethModule extends ReactContextBaseJavaModule {
     public void createAndSendTransaction(double nonce, String address, double amount, double gasLimit, double gasPrice, 
             Promise promise) {
 
-        Log.d("GETH", ndConfig.EthereumNetworkID.toString());
+        Log.d("GETH", ndConfig.getEthereumNetworkID().toString());
         promise.resolve(true);
 
         /*
